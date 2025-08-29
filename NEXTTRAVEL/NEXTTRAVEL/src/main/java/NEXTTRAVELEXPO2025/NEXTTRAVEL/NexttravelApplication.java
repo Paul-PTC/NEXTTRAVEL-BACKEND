c169bd1d000eb38b3631805f3a4b75e652085655
Paul-PTC <20210103@ricaldone.edu.sv>
2025-08-28 19:43:52 -0600

    Add files via upload

---
+++ NEXTTRAVEL/NEXTTRAVEL/src/main/java/NEXTTRAVELEXPO2025/NEXTTRAVEL/NexttravelApplication.java
@@ -0,0 +1,13 @@
+package NEXTTRAVELEXPO2025.NEXTTRAVEL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexttravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexttravelApplication.class, args);
+	}
+
+}
---
+++ NEXTTRAVEL/NEXTTRAVEL/src/test/java/NEXTTRAVELEXPO2025/NEXTTRAVEL/NexttravelApplicationTests.java
@@ -0,0 +1,13 @@
+package NEXTTRAVELEXPO2025.NEXTTRAVEL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexttravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
