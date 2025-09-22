6f8b922604d5ea4eefdbdb6fd364eb6cfff5d8e1
Zelayita <20240181@ricaldone.edu.sv>
2025-09-22 13:55:24 -0600

    Cambios de Argon2Password que se realizaron

--- NEXTTRAVEL/NEXTTRAVEL/src/main/java/NEXTTRAVELEXPO2025/NEXTTRAVEL/Config/Argon2Password.java
+++ NEXTTRAVEL/NEXTTRAVEL/src/main/java/NEXTTRAVELEXPO2025/NEXTTRAVEL/Config/Argon2Password.java
@@ -6,20 +6,31 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Argon2Password {
-
+    //Configuracion recomendada para Argon2id
     private static final int ITERATIONS = 10;
     private static final int MEMORY = 32768;
     private static final int PARALLELISM = 2;
 
-    //Crear una instancia de Argon2id
+    //Crear instancia de Argon2id
     private Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
 
+    /**
+     * Recibe el parametro password y encripta utilizando los valores definidos en la clase
+     * @param password sin encriptar
+     * @return, retorna una cadena basada en Argon2id
+     */
     public String EncryptPassword(String password){
         return argon2.hash(ITERATIONS, MEMORY, PARALLELISM, password);
     }
 
-    public boolean VerifyPassword(String hashDB, String contrasena) {
-        return argon2.verify(hashDB, contrasena);
+    /**
+     * El metodo recibe ambas contrasenas y mediante el metodo verify de Argon2, evalua si la contrasena es correcta.
+     * @param passwordDB password proveniente de la base de datos
+     * @param password password sin encriptacion, es el valor que el usuario ingresa en el login
+     * @return
+     */
+    public boolean VerifyPassword(String passwordDB, String password){
+        return argon2.verify(passwordDB, password);
     }
 
 }
